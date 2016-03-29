b3328108247642d8db1556b7aebea460a0e7fe28
Jessica Yao <jessicayao3981@gmail.com>
2016-03-29 11:26:16 -0400

    Add static pages controller

--- README.md
+++ README.md
@@ -39,8 +39,8 @@ progress. Put an x between the brackets for a checkmark: [x] -->
 **Objective:** Projects can be created, viewed and edited through the API
 
 - [X] create new project
-- [ ] create `Project` model
-- [ ] seed the database with a small amount of test data
+- [X] create `Project` model
+- [X] seed the database with a small amount of test data
 - [ ] CRUD API for projects (`ProjectsController`)
 - [ ] jBuilder views for projects
 - [ ] setup Webpack & Flux scaffold
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.create!(
+  title: "Make Sennacy website",
+  category: "Technology",
+  blurb: "All hail Sennacy. We must make a website for her!",
+  author_id: 1,
+  duration: 30,
+  goal: 500,
+)
+
+Project.create!(
+  title: "Mac and cheese",
+  category: "Food",
+  blurb: "I want to make the best mac and cheese in the world",
+  author_id: 2,
+  duration: 30,
+  goal: 100,
+)
+
+Project.create!(
+  title: "Mac and cheese",
+  category: "Food",
+  blurb: "I want to make the best mac and cheese in the world",
+  author_id: 2,
+  duration: 30,
+  goal: 100,
+)
+
+Project.create!(
+  title: "Gold and white dress",
+  category: "Fashion",
+  blurb: "That dress is totally white and gold. I want to make it but with real gold.",
+  author_id: 3,
+  duration: 30,
+  goal: 500000,
+)
+
+Project.create!(
+  title: "Sennacy the Game",
+  category: "Games",
+  blurb: "A game where you try to open as many doors as possible as a cat.",
+  author_id: 1,
+  duration: 30,
+  goal: 100000,
+)
