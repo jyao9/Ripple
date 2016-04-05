961ecc533c38649024787708df7efe6f3a1c2f79
Jessica Yao <jessicayao3981@gmail.com>
2016-04-05 14:59:10 -0400

    Create status bar on project icons

--- frontend/components/projects/index_item.jsx
+++ frontend/components/projects/index_item.jsx
@@ -10,10 +10,28 @@ var ProjectIndexItem = React.createClass({
   },
 
   render: function () {
+    var barWidth;
+
+    if (this.props !== undefined) {
+      barWidth = (this.props.project.status / this.props.project.goal) * 100;
+    } else {
+      barWidth = 100
+    }
+
+    var meterStyle = {
+      width: barWidth + "%"
+    };
+    // debugger
     return(
       <li className="project-index-item group" onClick={this.showDetail}>
+        <img className="icon" className="icon-image" src={this.props.project.image_url} />
         <div className="icon-title">{this.props.project.title}</div>
-        <img className="icon" className="preview-image" src={this.props.project.image_url} />
+        <div className="icon-author">{this.props.project.author_name}</div>
+        <div className="icon-blurb">{this.props.project.blurb}</div>
+        <br />
+        <div className="meter">
+          <span style={meterStyle}></span>
+        </div>
       </li>
     );
   }
