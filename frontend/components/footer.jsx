943ae293f883ae18b82f28cf619607972d988720
Jessica Yao <jessicayao3981@gmail.com>
2016-04-14 11:57:09 -0400

    Keep heroku active

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,10 +2,6 @@ var React = require('react');
 
 var Footer = React.createClass({
   render: function () {
-    var http = require("http");
-    setInterval(function() {
-        http.get("http://ripplestarter.herokuapp.com");
-    }, 1200000);
 
     return(
       <footer className="footer">
