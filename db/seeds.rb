ea0c7bc95fcb7d1c232e28e90befbd215bccb5a3
Jessica Yao <jessicayao3981@gmail.com>
2016-04-20 16:27:33 -0400

    Add more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,30 @@ User.create!(
   password: "password"
 )
 
+User.create!(
+  username: "Bobby Fillet",
+  password: "password"
+)
+
+User.create!(
+  username: "Gym Bunny",
+  password: "password"
+)
+
+User.create!(
+  username: "That Gamer",
+  password: "password"
+)
+
+User.create!(
+  username: "Amy",
+  password: "password"
+)
+
+User.create!(
+  username: "John Doe",
+  password: "password"
+)
 
 User.create!(
   username: "Guest",
@@ -72,6 +96,104 @@ Project.create!(
 
 project = Project.last
 file = File.open('app/assets/images/cat_game.jpg')
+project.image = file
+project.save!
+
+Project.create!(
+  title: "Beef Bacon",
+  category: "Food",
+  blurb: "I want to make bacon made from beef that taste even better than bacon made from pork. Come on guys, I know we can do this!",
+  author_id: 4,
+  duration: 15,
+  goal: 10000,
+)
+
+project = Project.last
+file = File.open('app/assets/images/bacon.jpg')
+project.image = file
+project.save!
+
+Project.create!(
+  title: "The Truth about Steak",
+  category: "Journalism",
+  blurb: "Help me to discover the truth about what steak is one of the best meals for breakfast, lunch and dinner. Let's not forget brunch.",
+  author_id: 4,
+  duration: 30,
+  goal: 30000,
+)
+
+project = Project.last
+file = File.open('app/assets/images/steak.jpg')
+project.image = file
+project.save!
+
+Project.create!(
+  title: "Pop Workout",
+  category: "Dance",
+  blurb: "Exercise is an important part of my life and I want to share it with the world. That's why I want to create a workout dance to all of today's catchiest pop songs.",
+  author_id: 5,
+  duration: 7,
+  goal: 500,
+)
+
+project = Project.last
+file = File.open('app/assets/images/workout_dance.jpg')
+project.image = file
+project.save!
+
+Project.create!(
+  title: "Bounce",
+  category: "Fashion",
+  blurb: "Join me in creating a new fashion line for workout clothes. The goal is to create clothes that are super comfortable and easy to work out and move around in.",
+  author_id: 5,
+  duration: 30,
+  goal: 500000,
+)
+
+project = Project.last
+file = File.open('app/assets/images/workout_clothes.jpg')
+project.image = file
+project.save!
+
+Project.create!(
+  title: "Quantum Chess",
+  category: "Games",
+  blurb: "You've seen the video. You know what must be done.",
+  author_id: 6,
+  duration: 60,
+  goal: 250000,
+)
+
+project = Project.last
+file = File.open('app/assets/images/chess.jpg')
+project.image = file
+project.save!
+
+Project.create!(
+  title: "Knitted Scarves",
+  category: "Crafts",
+  blurb: "I like to knit and people say I make great scarves. Support my brand.",
+  author_id: 7,
+  duration: 40,
+  goal: 3000,
+)
+
+project = Project.last
+file = File.open('app/assets/images/scarves.jpg')
+project.image = file
+project.save!
+
+Project.create!(
+  title: "Bambi Remake",
+  category: "Film",
+  blurb: "The original Bambi movie was just too sad. Let's remake it so that Bambi's mother doesn't die.",
+  author_id: 8,
+  duration: 25,
+  goal: 750000,
+)
+
+project = Project.last
+file = File.open('app/assets/images/bambi.jpg')
 project.image = file
 project.save!
 
@@ -157,4 +279,112 @@ Reward.create!(
   project_id: 4,
   value: 1000,
   description: "You will be able to suggest a skin for Sennacy."
+)
+
+Reward.create!(
+  project_id: 5,
+  value: 1,
+  description: "I just want to help out. No reward needed."
+)
+
+Reward.create!(
+  project_id: 5,
+  value: 100,
+  description: "You will receive 5 strips of the beef bacon."
+)
+
+Reward.create!(
+  project_id: 5,
+  value: 1000,
+  description: "You will receive 1 pack (20 strips) of the beef bacon."
+)
+
+Reward.create!(
+  project_id: 6,
+  value: 1,
+  description: "I just want to help out. No reward needed."
+)
+
+Reward.create!(
+  project_id: 6,
+  value: 100,
+  description: "Your name will be put on as a recognized donator."
+)
+
+Reward.create!(
+  project_id: 6,
+  value: 2000,
+  description: "You will get the chance to suggest a recipe to be featured."
+)
+
+Reward.create!(
+  project_id: 7,
+  value: 1,
+  description: "I just want to help out. No reward needed."
+)
+
+Reward.create!(
+  project_id: 7,
+  value: 100,
+  description: "You will receive a personalized thank you card."
+)
+
+Reward.create!(
+  project_id: 7,
+  value: 1000,
+  description: "You will receive a piece of clothing of your choice from the clothing line."
+)
+
+Reward.create!(
+  project_id: 8,
+  value: 1,
+  description: "I just want to help out. No reward needed."
+)
+
+Reward.create!(
+  project_id: 8,
+  value: 1000,
+  description: "Your name will be put on as a recognized donator."
+)
+
+Reward.create!(
+  project_id: 8,
+  value: 5000,
+  description: "You will be able to demo the game early."
+)
+
+Reward.create!(
+  project_id: 9,
+  value: 1,
+  description: "I just want to help out. No reward needed."
+)
+
+Reward.create!(
+  project_id: 9,
+  value: 100,
+  description: "You will receive a personalized thank you letter."
+)
+
+Reward.create!(
+  project_id: 9,
+  value: 500,
+  description: "You will receive a scarf."
+)
+
+Reward.create!(
+  project_id: 10,
+  value: 1,
+  description: "I just want to help out. No reward needed."
+)
+
+Reward.create!(
+  project_id: 10,
+  value: 100,
+  description: "Your name will be put on as a recognized donator."
+)
+
+Reward.create!(
+  project_id: 10,
+  value: 10000,
+  description: "You will be able voice act for the remake."
 )
